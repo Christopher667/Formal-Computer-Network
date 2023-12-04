633e1122737ad353d74f552b84044a2bad201906
Christopher667 <hellobjtu@gmail.com>
2023-12-04 22:02:14 +0800

    Improve ICMP Traceroute functions.

--- Traceroute.py
+++ Traceroute.py
@@ -27,9 +27,9 @@ TYPE_ICMP_UNREACHED = 3
 MAX_HOPS = 30  # set max hops-30
 TRIES = 3  # detect 3 times
 
-SEQUENCE = 0
-ICMP_FORMAT = '!bbHHh'
-DATA_FORMAT = '!d'
+SEQUENCE = 1
+ICMP_FORMAT = 'bbHHh'
+DATA_FORMAT = 'd'
 
 
 def checksum(string):
@@ -56,6 +56,42 @@ def checksum(string):
 
     return answer
 
+def receiveUdpPacket():
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp_socket.bind(("0.0.0.0", 33434))
+    try:
+        while True:
+            rec_packet, rec_address = udp_socket.recvfrom(1024)
+            print(f"Received UDP packet from {rec_address}: {rec_packet}")
+    except socket.error as e:
+        print(f"Socket error: {e}")
+    finally:
+        udp_socket.close()
+def receiveUdpPing(udp_socket, timeout):
+    udp_socket.settimeout(timeout)  # 确保超时时间正确设置
+    while True:
+        try:
+            start_time = time.time()
+            rec_packet, rec_address = udp_socket.recvfrom(1024)
+            time_delay = (time.time() - start_time) * 1000
+            print(f"Received UDP packet in {time_delay} ms")
+            if time_delay > timeout or not rec_packet:
+                print("No UDP packet received.")
+                return -1, -1, -1, '-1'
+
+            address = rec_address[0]
+            try:
+                host_info = socket.gethostbyaddr(address)
+            except socket.error as e:
+                host_name = '{0}'.format(address)
+            else:
+                host_name = '{0} ({1})'.format(address, host_info[0])
+
+            return time_delay, 11, 0, host_name
+        except socket.timeout:
+            print("UDP socket timeout. No packet received.")
+            return -1, -1, -1, '-1'
+
 
 def receiveOnePing(icmpSocket, destinationAddress, ID, timeout):
     while True:
@@ -63,7 +99,7 @@ def receiveOnePing(icmpSocket, destinationAddress, ID, timeout):
         select_object = select.select([icmpSocket], [], [], timeout)
         run_time = time.time() - start_time
 
-        if select_object[0] == [] or run_time > timeout:
+        if run_time > timeout or run_time == 0:
             return -1, -1, -1, '-1'
 
         rec_packet, rec_address = icmpSocket.recvfrom(1024)
@@ -105,7 +141,7 @@ def sendOnePing(socket, destinationAddress, ID, protocol):
     # 4. Send packet using socket
     try:
         if protocol == 'icmp':
-            socket.sendto(packet, (destinationAddress, 1))
+            socket.sendto(packet, (destinationAddress, 80))
         elif protocol == 'udp':
             socket.sendto(packet, (destinationAddress, 33434))
 
@@ -119,10 +155,11 @@ def doOnePing(destinationAddress, timeout, ttl, protocol):
     icmp_socket.setsockopt(socket.IPPROTO_IP, socket.IP_TTL, struct.pack('I', ttl))
     icmp_socket.settimeout(timeout)
     # Bind port
-    icmp_socket.bind(("", 1))
+    #icmp_socket.bind(("", 1))
     #
     # Build udp_socket
     udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     udp_socket.setsockopt(socket.IPPROTO_IP, socket.IP_TTL, struct.pack('I', ttl))
     udp_socket.settimeout(timeout)
     #udp_socket.bind(("", 8001))
@@ -131,33 +168,28 @@ def doOnePing(destinationAddress, timeout, ttl, protocol):
     my_id = os.getpid() & 0xFFFF
 
     if protocol == 'icmp':
-        # Close udp_socket
-        udp_socket.close()
         sendOnePing(icmp_socket, destinationAddress, my_id, protocol)
-
     elif protocol == 'udp':
-        # Close icmp_socket
-        icmp_socket.close()
         sendOnePing(udp_socket, destinationAddress, my_id, protocol)
-
-    # 5. Return total network delay
-    time_delay, rec_type, rec_code, host_name = receiveOnePing(udp_socket, destinationAddress, my_id, timeout)
+        time.sleep(0.1)
+    udp_socket.close()
+    time_delay, rec_type, rec_code, host_name = receiveOnePing(icmp_socket, destinationAddress, my_id, timeout)
+    icmp_socket.close()
     return time_delay, rec_type, rec_code, host_name
 
-
 def traceroute(host, timeout, protocol):
-    for ttl in range(1, MAX_HOPS + 1):
+    print("Host: %s  [%s]" %(host, socket.gethostbyname(host)))
+    for ttl in range(1, MAX_HOPS ):
         host_name = ''
         send = 0
         print("\nMeasurement %d: " % ttl)
 
         for hops in range(0, 3):
             delay, rec_type, rec_code, ip_name = doOnePing(host, timeout, ttl, protocol)
+            time.sleep(0.5)
+            #print("\nrec_type: %d, rec_code: %d"%(rec_type, rec_code))
             if delay == -1 and rec_type == -1 and rec_code == -1:
                 print("  *  ", end='\t')
-            elif rec_type == 0:
-                print("Finish!")
-                return
             elif rec_type == 3 and rec_code == 0:
                 print("Network Unreachable!")
                 break
@@ -168,12 +200,18 @@ def traceroute(host, timeout, protocol):
                 print("Protocol Unreachable!")
                 break
             elif rec_type == 3 and rec_code == 3:
-                print("Port Unreachable!")
-                break
-            elif rec_type == 11:
+                print("Port Unreachable! Finish!")
+                return
+            elif rec_type == 11 and rec_code == 0:
                 print("%3.0dms" % (delay*1000), end='\t')
                 host_name = ip_name
                 send += 1
+            elif rec_type == 0 and rec_code == 0:
+                print("%3.0dms [ip: %s]" % (delay * 1000, ip_name), end='\t')
+                print("Finish!")
+                return
+            else:
+                print("Error!")
 
         if send != 0:
             print("Host: %s" % host_name, end='\t')
